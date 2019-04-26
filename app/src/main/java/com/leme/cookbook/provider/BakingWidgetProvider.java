bd1dedbcc250d16e4e55667bbd72b21d2bff790b
felleme <felleme@prestadores.amil.com.br>
2019-04-26 16:10:23 -0300

    create initial widget and intent action to open app

---
+++ app/src/main/java/com/leme/cookbook/provider/BakingWidgetProvider.java
@@ -0,0 +1,48 @@
+package com.leme.cookbook.provider;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import com.leme.cookbook.MainActivity;
+import com.leme.cookbook.R;
+
+public class BakingWidgetProvider extends AppWidgetProvider {
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.baking_widget);
+        views.setOnClickPendingIntent(R.id.widget_baking_chef_image, pendingIntent);
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        // Perform any action when one or more AppWidget instances have been deleted
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Perform any action when an AppWidget for this provider is instantiated
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Perform any action when the last AppWidget instance for this provider is deleted
+    }
+}
