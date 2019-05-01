1a7bdd9ae812109099bda022b0c13212c1df41f3
lemefe21 <lemefe21@gmail.com>
2019-05-01 17:28:39 -0300

    implement exoPlayer and run url videos in steps fragment

--- app/src/main/java/com/leme/cookbook/fragment/StepsFragment.java
+++ app/src/main/java/com/leme/cookbook/fragment/StepsFragment.java
@@ -1,6 +1,8 @@
 package com.leme.cookbook.fragment;
 
 
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,6 +11,18 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 import com.leme.cookbook.R;
 import com.leme.cookbook.model.Step;
 
@@ -21,6 +35,11 @@ public class StepsFragment extends Fragment {
 
     private List<Step> steps;
     private int stepSequence = 0;
+
+    SimpleExoPlayer mExoPlayer;
+
+    @BindView(R.id.step_player_view)
+    SimpleExoPlayerView mPlayerView;
 
     @BindView(R.id.step_position_sequence)
     TextView mStepPositionSequence;
@@ -61,6 +80,7 @@ public class StepsFragment extends Fragment {
         mStepBtnPrevious.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                releasePlayer();
                 stepSequence--;
                 configStepFlow();
             }
@@ -69,10 +89,13 @@ public class StepsFragment extends Fragment {
         mStepBtnNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                releasePlayer();
                 stepSequence++;
                 configStepFlow();
             }
         });
+
+        mPlayerView.setDefaultArtwork(BitmapFactory.decodeResource(getResources(), R.drawable.ic_play_circle));
 
         return view;
     }
@@ -101,5 +124,43 @@ public class StepsFragment extends Fragment {
             mStepBtnNext.setVisibility(View.VISIBLE);
             mStepBtnPrevious.setVisibility(View.VISIBLE);
         }
+
+        if(!step.getVideoURL().equalsIgnoreCase("")) {
+            mPlayerView.setVisibility(View.VISIBLE);
+            mStepVideoUrl.setVisibility(View.GONE);
+            initializePlayer(Uri.parse(step.getVideoURL()));
+        } else {
+            mPlayerView.setVisibility(View.GONE);
+        }
+    }
+
+    private void initializePlayer(Uri mediaUri) {
+        if(mExoPlayer == null) {
+
+            TrackSelector trackSelector = new DefaultTrackSelector();
+            LoadControl loadControl = new DefaultLoadControl();
+            mExoPlayer = ExoPlayerFactory.newSimpleInstance(getContext(), trackSelector, loadControl);
+            mPlayerView.setPlayer(mExoPlayer);
+            // Prepare the MediaSource.
+            String userAgent = Util.getUserAgent(getContext(), "ClassicalMusicQuiz");
+            MediaSource mediaSource = new ExtractorMediaSource(mediaUri, new DefaultDataSourceFactory(
+                    getContext(), userAgent), new DefaultExtractorsFactory(), null, null);
+            mExoPlayer.prepare(mediaSource);
+            mExoPlayer.setPlayWhenReady(true);
+        }
+    }
+
+    private void releasePlayer() {
+        if(mExoPlayer != null) {
+            mExoPlayer.stop();
+            mExoPlayer.release();
+            mExoPlayer = null;
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        releasePlayer();
     }
 }
